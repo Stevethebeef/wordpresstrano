a48d9348e0b58dec2f6de82e01cc2cd4c9893bd0
Toby Herbert <tobyherbert@synappsdev.co.uk>
2015-08-15 16:16:06 +0100

    Moved hooks into a file

--- lib/capistrano/wordpress.rb
+++ lib/capistrano/wordpress.rb
@@ -3,7 +3,7 @@ require "securerandom"
 require "time"
 
 # Load all helpers and capistrano tasks
-["wordpress/helpers/**/*.rb", "wordpress/tasks/**/*.rake"].each do |glob|
+["wordpress/helpers/**/*.rb", "wordpress/tasks/**/*.rake", "wordpress/hooks.rb"].each do |glob|
   Dir.glob(File.expand_path(File.join('..', glob), __FILE__)).each do |file_path|
     load file_path
   end
@@ -14,13 +14,3 @@ set :linked_files, fetch(:linked_files, []).push(".htaccess", "robots.txt", "wp-
 
 # Tell capistrano about directories we want linked into releases
 set :linked_dirs, fetch(:linked_dirs, []).push("wp-content/uploads")
-
-# Deploy our resources before running the deploy command
-before "deploy", "deploy:resources"
-
-# Download the WordPress core to the new release
-after "deploy:symlink:release", "wp:core:download"
-
-# Link the new release into the Website Root and set permissions on it
-after "deploy:finished", "webroot:setperms"
-after "deploy:finished", "webroot:symlink"
---
+++ lib/capistrano/wordpress/hooks.rb
@@ -0,0 +1,16 @@
+# Check for binaries before Deploy tasks
+before "deploy:check", "binaries:check"
+
+# Check for binaries before WordPress tasks
+before "wp:core:download", "binaries:check"
+before "wp:core:remove", "binaries:check"
+
+# Deploy resources before deploying
+#before "deploy", "deploy:resources"
+
+# Download the WordPress core after downloading a release
+#after "deploy:symlink:release", "wp:core:download"
+
+# Link the current release into the website root after deploying
+#after "deploy:finished", "webroot:setperms"
+#after "deploy:finished", "webroot:symlink"

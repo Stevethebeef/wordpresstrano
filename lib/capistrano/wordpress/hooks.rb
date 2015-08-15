157a3068eb414233bcafe9943a7bee801e645944
Toby Herbert <tobyherbert@synappsdev.co.uk>
2015-08-15 19:23:21 +0100

    Rearranged hooks.rb file

--- lib/capistrano/wordpress/hooks.rb
+++ lib/capistrano/wordpress/hooks.rb
@@ -7,9 +7,6 @@ before "deploy", "deploy:resources"
 # Check directories before deploying resources
 before "deploy:resources", "deploy:check:directories"
 
-# Push the local database before finishing a deployment
-#before "deploy:finishing", "db:push"
-
 # Remove the existing WordPress core before downloading a new one
 before "wp:core:download", "wp:core:remove"
 
@@ -25,8 +22,11 @@ before "config:generate", "deploy:check:directories"
 # Check directories before generating a robots.txt file
 before "robots:generate", "deploy:check:directories"
 
-# Download the WordPress core after symlinking the release
-after "deploy:symlink:release", "wp:core:download"
+# Download the WordPress core before symlinking the release
+before "deploy:symlink:release", "wp:core:download"
+
+# Push the local database before finishing a deployment
+#before "deploy:symlink:release", "db:push"
 
 # Link the new release into the website root
 #after "deploy:finished", "webroot:symlink"

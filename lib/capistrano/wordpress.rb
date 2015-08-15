ffab1aa3daf30d47dbe5853e9a499dbbb3b6c5d0
Toby Herbert <tobyherbert@synappsdev.co.uk>
2015-08-15 11:42:23 +0100

    The main wordpress.rb file now loads ruby files from the helpers directory

--- lib/capistrano/wordpress.rb
+++ lib/capistrano/wordpress.rb
@@ -1,5 +1,5 @@
-# Load all ruby and rake files
-["wordpress/**/*.rb", "wordpress/tasks/**/*.rake"].each do |glob|
+# Load all helpers and capistrano tasks
+["wordpress/helpers/**/*.rb", "wordpress/tasks/**/*.rake"].each do |glob|
   Dir.glob(File.expand_path(File.join('..', glob), __FILE__)).each do |file_path|
     load file_path
   end

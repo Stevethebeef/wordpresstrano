0622b79fc2e4ec181e8021e70d0f19d008dfdc06
Toby Herbert <tobyherbert@synappsdev.co.uk>
2015-08-15 17:56:18 +0100

    Removed htaccess from linked_files and uploads from linked_dirs

--- lib/capistrano/wordpress.rb
+++ lib/capistrano/wordpress.rb
@@ -10,7 +10,4 @@ require "time"
 end
 
 # Tell capistrano about files we want linked into releases
-set :linked_files, fetch(:linked_files, []).push(".htaccess", "robots.txt", "wp-config.php")
-
-# Tell capistrano about directories we want linked into releases
-set :linked_dirs, fetch(:linked_dirs, []).push("wp-content/uploads")
+set :linked_files, fetch(:linked_files, []).push("robots.txt", "wp-config.php")

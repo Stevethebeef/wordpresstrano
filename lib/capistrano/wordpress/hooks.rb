0667871e319d064b7607d7a591a4f20a574d8159
Toby Herbert <tobyherbert@synappsdev.co.uk>
2015-08-16 12:15:37 +0100

    Restructured hooks again

--- lib/capistrano/wordpress/hooks.rb
+++ lib/capistrano/wordpress/hooks.rb
@@ -1,66 +1,42 @@
-# Check binaries before deploying
+# Check binaries before performing tasks
 before "deploy", "binaries:check"
+before "wp:core:download", "binaries:check"
+before "wp:core:remove", "binaries:check"
+
+# Check directories before performing tasks
+before "config:generate", "deploy:check:directories"
+before "db:create", "deploy:check:directories"
+before "db:push", "deploy:check:directories"
+before "deploy:shared_configs", "deploy:check:directories"
+before "htaccess:push", "deploy:check:directories"
+before "robots:generate", "deploy:check:directories"
+before "uploads:push", "deploy:check:directories"
 
 # Deploy shared configuration files before deploying
 before "deploy", "deploy:shared_configs"
-
-# Check directories before deploying shared configurations
-before "deploy:shared_configs", "deploy:check:directories"
-
-# Remove the existing WordPress core before downloading a new one
-before "wp:core:download", "wp:core:remove"
-
-# Check binaries before downloading the WordPress core
-before "wp:core:download", "binaries:check"
-
-# Check binaries before removing the WordPress core
-before "wp:core:remove", "binaries:check"
-
-# Check directories before generating a wp-config.php file
-before "config:generate", "deploy:check:directories"
-
-# Check directories before generating a robots.txt file
-before "robots:generate", "deploy:check:directories"
 
 # Load the local WordPress version so that when downloading the
 # WordPress core on a remote server, the version matches the local installation.
 before "deploy:updated", "wp:core:load_local_version"
 
-# Download the WordPress core before calling updated on the release
+# Remove the existing WordPress core before downloading a new one
+before "wp:core:download", "wp:core:remove"
+
+# Download the WordPress core files before finishing deploy:updated
 before "deploy:updated", "wp:core:download"
 
-# Check directories before pushing a htaccess file
-before "htaccess:push", "deploy:check:directories"
-
-# Check directories before pushing a uploads directory
-before "uploads:push", "deploy:check:directories"
-
-# Push the local .htaccess file after the deployment
-after "deploy:updated", "htaccess:push"
-
-# Push the local uploads directory after the deployment
-after "deploy:updated", "uploads:push"
-
-# Push the local database after the deployment
-#after "deploy:updated", "db:push"
-
-# Rollback the database after the deployment has reverted
-#after "deploy:reverted", "db:rollback"
-
-# Link the new release into the website root
-after "deploy:finished", "webroot:symlink"
-
-# Set permissions on the website root
+# Set permissions on the resources after deploying them
+after "config:generate", "config:setperms"
+after "htaccess:push", "htaccess:setperms"
+after "robots:generate", "robots:setperms"
+after "uploads:push", "uploads:setperms"
 after "deploy:finished", "webroot:setperms"
 
-# Set permissions on the wp-config.php file after generating
-after "config:generate", "config:setperms"
+# Push the local resources after finishing deploy:updated
+#after "deploy:reverted", "db:rollback"
+after "deploy:updated", "db:push"
+after "deploy:updated", "htaccess:push"
+after "deploy:updated", "uploads:push"
 
-# Set permissions on the robots.txt file after generating
-after "robots:generate", "robots:setperms"
-
-# Set permissions on the .htaccess file after pushing
-after "htaccess:push", "htaccess:setperms"
-
-# Set permissions on the uploads directory after pushing
-after "uploads:push", "uploads:setperms"
+# Link the release into the website root
+after "deploy:finished", "webroot:symlink"
